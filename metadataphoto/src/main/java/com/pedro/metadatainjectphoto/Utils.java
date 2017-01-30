dbb7c1f61e5f10926ccf91b35e47ed0934f06e34
pedro <pedrosanchez@droiders.com>
2017-01-30 12:47:07 +0100

    add fb compatibility

--- metadataphoto/src/main/java/com/pedro/metadatainjectphoto/Utils.java
+++ metadataphoto/src/main/java/com/pedro/metadatainjectphoto/Utils.java
@@ -2,12 +2,10 @@ package com.pedro.metadatainjectphoto;
 
 import android.content.Context;
 import android.util.Log;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -52,6 +50,9 @@ public class Utils {
     exif.addExifField(ExifTag.DATE_TIME_ORIGINAL, FieldType.ASCII, dateFormat.format(date));
     exif.addExifField(ExifTag.DATE_TIME_DIGITIZED, FieldType.ASCII, dateFormat.format(date));
     exif.addImageField(TiffTag.SOFTWARE, FieldType.ASCII, "pedroSG94");
+    //need it for some web compatibility like facebook
+    exif.addImageField(TiffTag.MODEL, FieldType.ASCII, "pedroSG94");
+    exif.addImageField(TiffTag.MAKE, FieldType.ASCII, "pedroSG94");
     return exif;
   }
 }
